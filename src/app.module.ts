581baf318982cf106873697235c4886871000a92
Kyung Lee <kyung.lee.official@outlook.com>
2023-01-12 00:09:49 +0800

    Update: generate roles and permissions resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+import { RolesModule } from './roles/roles.module';
+import { PermissionsModule } from './permissions/permissions.module';
 
 @Module({
 	imports: [
@@ -21,6 +23,8 @@ import { ConfigModule } from "@nestjs/config";
 		}),
 		UsersModule,
 		AuthModule,
+		RolesModule,
+		PermissionsModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
