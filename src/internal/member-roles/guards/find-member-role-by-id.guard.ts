741c6884789053e66142116e509bab36f480e2a6
Kyung Lee <kyung.lee.official@outlook.com>
2024-10-28 17:52:27 +0800

    chore: update some guards

--- src/internal/member-roles/guards/find-member-role-by-id.guard.ts
+++ src/internal/member-roles/guards/find-member-role-by-id.guard.ts
@@ -1,43 +1,44 @@
 import {
-	Injectable,
 	CanActivate,
 	ExecutionContext,
+	Injectable,
 	NotFoundException,
 } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { GRPC as Cerbos } from "@cerbos/grpc";
+import { PrismaService } from "../../../prisma/prisma.service";
 import { getCerbosPrincipal } from "src/utils/data";
+import { GRPC as Cerbos } from "@cerbos/grpc";
 
 const cerbos = new Cerbos(process.env.CERBOS_HOST as string, { tls: false });
 
 @Injectable()
-export class FindMeGuard implements CanActivate {
+export class FindMemberRoleByIdGuard implements CanActivate {
 	constructor(private readonly prismaService: PrismaService) {}
 
-	async canActivate(context: ExecutionContext): Promise<boolean> {
+	async canActivate(context: ExecutionContext) {
 		const req = context.switchToHttp().getRequest();
 		const requester = req.requester;
 		const principal = getCerbosPrincipal(requester);
 
-		const member = await this.prismaService.member.findUnique({
+		const action = "read";
+
+		const role = await this.prismaService.memberRole.findUnique({
 			where: {
-				email: req.jwtPayload.email,
+				id: req.param.id,
 			},
 		});
-		if (!member) {
-			throw new NotFoundException("Member not found");
+		if (!role) {
+			throw new NotFoundException("Role not found");
 		}
+
 		const resource = {
-			kind: "internal:members",
-			id: member.id,
+			kind: "internal:member-roles",
+			id: role.id,
 			attributes: {
-				...member,
-				createdAt: member.createdAt.toISOString(),
-				updatedAt: member.updatedAt.toISOString(),
+				...role,
+				createdAt: role.createdAt.toISOString(),
+				updatedAt: role.updatedAt.toISOString(),
 			},
 		};
-
-		const action = "read";
 
 		const cerbosObject = {
 			principal: {
