2202a002ecf5cdeb651a78799612f760dadbf96a
Kyung Lee <kyung.lee.official@outlook.com>
2024-11-20 16:32:50 +0800

    fix: fix create-role.dto.ts regex error

--- src/internal/roles/dto/create-role.dto.ts
+++ src/internal/roles/dto/create-role.dto.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createRoleSchema = z.object({
-	id: z.string().regex(/[a-z0-9\._-]+/),
+	id: z.string().regex(/^[a-z0-9\._-]+$/),
 	name: z.string().min(3).max(32),
 });
 
