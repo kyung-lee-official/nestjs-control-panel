f608ad3d01a3faffb468501c321ac0d944192f90
Kyung Lee <kyung.lee.official@outlook.com>
2024-11-29 10:54:49 +0800

    chore: remove verified guard for ctb-manual

--- src/chitubox-manual-feedbacks/chitubox-manual-feedbacks.controller.ts
+++ src/chitubox-manual-feedbacks/chitubox-manual-feedbacks.controller.ts
@@ -9,7 +9,6 @@ import {
 } from "@nestjs/common";
 import { ChituboxManualFeedbacksService } from "./chitubox-manual-feedbacks.service";
 import { Iso8601DateRangeDto } from "./dto/iso8601-date-range.dto";
-import { IsVerifiedGuard } from "../internal/members/guards/is-verified.guard";
 import { ChituboxManualFeedback } from "@prisma/client";
 import { JwtGuard } from "src/internal/authentication/guards/jwt.guard";
 import { CreateChituboxManualFeedbackDto } from "./dto/create-chitubox-manual-feedback.dto";
@@ -32,10 +31,7 @@ export class ChituboxManualFeedbacksController {
 		);
 	}
 
-	// @UseGuards(PermissionsGuard)
-	@UseGuards(IsVerifiedGuard)
 	@UseGuards(JwtGuard)
-	// @RequiredPermissions(Permissions.FIND_CHITUBOX_MANUAL_FEEDBACKS)
 	@Get()
 	find(
 		@Query()
