01b64010e431a327ec2b7314446a5ebd01a0ca50
Kyung Lee <kyung.lee.official@outlook.com>
2025-02-28 10:19:55 +0800

    feat: update sns-crawler.prisma

---
+++ prisma/migrations/20250228021832_update_sns_crawler_youtube/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "YouTubeVideoInfoSearchKeyword" (
+    "id" SERIAL NOT NULL,
+    "keyword" TEXT NOT NULL,
+
+    CONSTRAINT "YouTubeVideoInfoSearchKeyword_pkey" PRIMARY KEY ("id")
+);
