ae76657c4a12ac23982db042acac10f300ab110b
Kyung Lee <kyung.lee.official@outlook.com>
2025-02-28 10:43:28 +0800

    fix: ...

--- src/internal/applications/sns-crawler/youtube-data-collector/youtube-data-collector.module.ts
+++ src/internal/applications/sns-crawler/youtube-data-collector/youtube-data-collector.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { YoutubeDataCollectorService } from "./youtube-data-collector.service";
 import { YoutubeDataCollectorController } from "./youtube-data-collector.controller";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-	imports: [PrismaService],
+	imports: [PrismaModule],
 	controllers: [YoutubeDataCollectorController],
 	providers: [YoutubeDataCollectorService],
 })
