3fbd5a609da64ecb0f70880ff4cc06c09cbabb3e
Kyung Lee <kyung.lee.official@outlook.com>
2025-04-03 17:13:56 +0800

    fix: change score fields to Float type in Event and EventTemplate models

--- src/internal/applications/performances/events/dto/update-event.dto.ts
+++ src/internal/applications/performances/events/dto/update-event.dto.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const updateEventDtoSchema = z.object({
-	score: z.number().min(0),
+	score: z.number(),
 	/* amount can only be added after the event is created, default is 1 */
 	amount: z.number().min(0).optional(),
-	description: z.string(),
+	description: z.string().min(1, "description is required"),
 	/* attachments can only be added after the event is created */
 	// attachments: z.array(z.string()),
 });
